0b540e8a52f9b87ac3a5a1a74698a03378546af5
Seryn19 <nabilaputri1909@gmail.com>
2026-05-05 20:00:43 +0800

    Add files via upload

---
+++ app/ABC/page.tsx
@@ -0,0 +1,9 @@
+export default function Blog() {
+    return (
+        <div>
+            <p>Ini adalah halaman ABC lima dasar!</p>
+            <input type="text" placeholder="Masukkan teks di sini" />
+            <button>Click Me</button>
+        </div>
+    );
+}
