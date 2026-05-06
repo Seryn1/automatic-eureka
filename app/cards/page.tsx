ec448238de061d637537ea14ffcb854d3b23f14d
Seryn19 <nabilaputri1909@gmail.com>
2026-05-06 21:56:35 +0800

    Add files via upload

---
+++ app/cards/page.tsx
@@ -0,0 +1,12 @@
+export default function CardPage() {
+  return (
+    <div className="flex justify-between p-5 items-center">
+        <h2 className="font-bold">Explore</h2>
+            <button className=" bg-gray-300 p-1 rounded-1g">
+            </button>
+              <div className="bg-blue">
+
+              </div>
+            </div>
+  );
+}
